3d07df51f4ac8d3267ff0422210ffe0b1cf917c9
Gllp2 <laureanogllp2@gmail.com>
2025-07-21 12:20:21 +0100

    uploaded underConstruction waiting page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import UnderConstruction from './components/underConstruction';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <UnderConstruction />
     </>
   );
 }
---
+++ src/components/underConstruction.jsx
@@ -0,0 +1,15 @@
+import '../styles/underConstruction.css';
+
+export default function UnderConstruction() {
+  return (
+    <>
+    <div className="under-construction">
+      <h1>Under Construction</h1>
+    </div>
+      <br />
+      <div className="under-construction-text">
+      <p>Please check back later.</p>
+      </div>
+    </>
+  );
+}
